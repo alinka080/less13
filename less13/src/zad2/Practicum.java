d502c1359294667bb7245c4da65e0e6670006c75
alinka080 <alinalis080@gmail.com>
2024-12-27 12:26:57 +0300

    Add files via upload

---
+++ less13/src/zad2/Practicum.java
@@ -0,0 +1,18 @@
+package zad2;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practicum {
+    public static void main(String[] args) {
+        // ключ — название задачи, значение — выполнена эта задача или ещё нет
+        Map<String, Boolean> todos = new HashMap<>();
+
+        todos.put("Выучить Java", true);
+        todos.put("Найти классную работу", false);
+        todos.clear(); // дополните код, чтобы очистить список задач
+
+        boolean hasTodos = todos.containsKey("Кокос");
+
+        System.out.println("Todo-список пуст? Ответ: " + todos.isEmpty());// дополните кодом, который даст верный ответ
+    }
+}
